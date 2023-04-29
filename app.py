33f1bbdef89972dd0b01a4c2b81307674cd8a38c
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-04-29 17:30:10 -0400

    added chat history functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 from flask import Flask, render_template, request, jsonify  
 import openai  
 from openai.error import RateLimitError  
+from ast import literal_eval
 
 load_dotenv()
   
@@ -15,20 +16,21 @@ def index():
     return render_template('index.html')  
   
 @app.route('/gpt4', methods=['GET', 'POST'])  
-def gpt4():  
-    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']  
-    messages = [{"role": "user", "content": user_input}]  
-  
-    try:  
-        response = openai.ChatCompletion.create(  
-            model="gpt-4",  
-            messages=messages  
-        )  
-        content = response.choices[0].message["content"]  
-    except RateLimitError:  
-        content = "The server is experiencing a high volume of requests. Please try again later."  
-  
-    return jsonify(content=content)  
+def gpt4():
+    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+    messages = literal_eval(request.form['messages'])  # parse the messages list from the request
+    messages.append({"role": "user", "content": user_input})  # add the current user message to the messages list
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages
+        )
+        content = response.choices[0].message["content"]
+    except RateLimitError:
+        content = "The server is experiencing a high volume of requests. Please try again later."
+
+    return jsonify(content=content)
   
 if __name__ == '__main__':  
     app.run(debug=True)
