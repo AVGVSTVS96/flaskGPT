df6192baeaa7f609950224eb0277d5fb155f8be0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-04-30 23:41:04 -0400

    v0.2 added streaming, not working
    response successfully received from GPT, successfully broken up into chunks, chunks are all displayed not concatenated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response, json
 import openai
 from openai.error import RateLimitError
 
@@ -15,23 +15,38 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/gpt4', methods=['GET', 'POST'])
-def gpt4():
-    data = request.json
+def generate(data):
     user_input = data.get('user_input')
     messages = data.get('messages', [])
-    messages = [{"role": "system", "content": "respond with only two words and two emojis"}] + messages
     messages.append({"role": "user", "content": user_input})
+
+    assistant_response = []
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-4",
-            messages=messages
+            messages=messages,
+            stream=True
         )
-        content = response.choices[0].message["content"]
-    except RateLimitError:
-        content = "The server is experiencing a high volume of requests. Please try again later."
 
-    return jsonify(content=content)
+        for chunk in response:
+            content = chunk['choices'][0]['delta']
+            if isinstance(content, str):
+                assistant_response.append({"content": content})
+            else:
+                assistant_response.append({"content": str(content)})
+
+    except RateLimitError:
+        assistant_response.append({"content": "The server is experiencing a high volume of requests. Please try again later."})
+
+    return jsonify(assistant_response)
+
+
+@app.route('/gpt4', methods=['POST'])
+def gpt4():
+    data = request.json
+    assistant_response = generate(data)
+    return assistant_response
 
 
 if __name__ == '__main__':
