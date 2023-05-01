916637521b44c7fafb65fc5c0a1099acb0725f6a
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-01 16:35:24 -0400

    chunks - simplified code, moved chunks to def gpt4
    cleaned up def generate function, moved "if isinstance(content, str):..." to def gpt4, chunks are processed in their own function

--- app.py
+++ app.py
@@ -33,12 +33,10 @@ def process_chunks(concatenated_chunks):
 def generate(data):
     user_input = data.get('user_input')
     messages = data.get('messages', [])
-    messages = [{"role": "system",
-                 "content": "respond with only two words and two emojis"}] + messages
+    messages = [{"role": "system", "content": "respond with only two words and two emojis"}] + messages
     messages.append({"role": "user", "content": user_input})
 
     assistant_response = []
-    current_chunk = ""
 
     try:
         response = openai.ChatCompletion.create(
@@ -49,24 +47,14 @@ def generate(data):
 
         for chunk in response:
             content = chunk['choices'][0]['delta']
-            if isinstance(content, str):
-                current_chunk += content
-            else:
-                current_chunk += str(content)
-
-            # Check if the current chunk is a complete JSON object
-            try:
-                json.loads(current_chunk)
-                assistant_response.append({"content": current_chunk})
-                current_chunk = ""
-            except json.JSONDecodeError:
-                pass
+            assistant_response.append({"content": content})
 
     except RateLimitError:
         assistant_response.append(
             {"content": "The server is experiencing a high volume of requests. Please try again later."})
 
     return jsonify(assistant_response)
+
 
 
 @app.route('/gpt4', methods=['POST'])
@@ -77,12 +65,18 @@ def gpt4():
     # Concatenate the chunks
     concatenated_chunks = ""
     for chunk in assistant_response.json:
-        concatenated_chunks += chunk.get("content", "")
+        content = chunk.get("content", "")
+        if isinstance(content, str):
+            concatenated_chunks += content
+        else:
+            concatenated_chunks += json.dumps(content)
 
     # Now process the concatenated chunks
     processed_response = process_chunks(concatenated_chunks)
 
     return jsonify(processed_response)
+
+
 
 
 if __name__ == '__main__':
