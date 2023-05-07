7e5c01750681155a4908302852785a665709f53f
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-07 01:41:42 -0400

    Simplified code, refactored /gpt4 route for POST
    Removed support for GET requests and conditional handling in /gpt4 route to streamline POST request processing.
    Additionally, eliminated the unused assistant_response list for increased code simplicity.

--- app.py
+++ app.py
@@ -10,16 +10,12 @@ load_dotenv()
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 def generate(messages):
     def stream():
-        assistant_response = []
-
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -31,28 +27,19 @@ def generate(messages):
                 content = chunk['choices'][0]['delta'].get('content', '')
                 if content:
                     yield content
-                    assistant_response.append({"content": content})
 
         except RateLimitError:
-            assistant_response.append(
-                {"content": "The server is experiencing a high volume of requests. Please try again later."})
-            yield assistant_response[-1]["content"]
+            yield "The server is experiencing a high volume of requests. Please try again later."
 
         yield '[DONE]'
 
     return stream()
 
-
-# <-- Change this line to allow POST requests
-@app.route('/gpt4', methods=['GET', 'POST'])
+@app.route('/gpt4', methods=['POST'])
 def gpt4():
-    if request.method == 'POST':
-        data = request.get_json()
-        user_input = data.get('user_input')
-        messages = data.get('messages', [])
-    else:
-        user_input = request.args.get('user_input')
-        messages = json.loads(request.args.get('messages', '[]'))
+    data = request.get_json()
+    user_input = data.get('user_input')
+    messages = data.get('messages', [])
 
     messages = [{"role": "system",
                  "content": "respond with only two words and two emojis"}] + messages
@@ -60,7 +47,6 @@ def gpt4():
 
     assistant_response = generate(messages)
     return Response(assistant_response, mimetype='text/event-stream')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
