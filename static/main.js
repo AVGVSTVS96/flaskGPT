03be3e963170d128887c620240376035043afc43
AVGVSTVS96 <null>
2023-05-22 20:39:42 +0000

    🔄 synced local 'static/main.js' with remote 'static/main.js'

--- static/main.js
+++ static/main.js
@@ -1,18 +1,23 @@
 const chatMessagesDiv = document.getElementById("chat-messages");
 const userInputElem = document.getElementById("user-input");
-
 const settingsButton = document.getElementById("settings-toggle");
 const settingsDropdown = document.querySelector(".settings-dropdown");
-
 const modelToggle = document.getElementById("model-toggle");
 const modelLabel = document.getElementById("model-label");
-let modelName = modelToggle.checked ? "gpt-4" : "gpt-3.5-turbo";
 
+// State variables
+let modelName = modelToggle.checked ? "gpt-4" : "gpt-3.5-turbo";
 let messages = [];
 let systemMessageRef = null;
 let autoScrollState = true;
 
-modelToggle.addEventListener("change", function () {
+function toggleDropdownDisplay() {
+  settingsDropdown.style.display =
+    settingsDropdown.style.display === "block" ? "none" : "block";
+}
+
+// Event listener functions
+function handleModelToggle() {
   if (modelToggle.checked) {
     modelLabel.textContent = "GPT-4";
     modelName = "gpt-4";
@@ -20,14 +25,9 @@ modelToggle.addEventListener("change", function () {
     modelLabel.textContent = "GPT-3.5";
     modelName = "gpt-3.5-turbo";
   }
-});
-
-function toggleDropdownDisplay() {
-  settingsDropdown.style.display =
-    settingsDropdown.style.display === "block" ? "none" : "block";
 }
 
-document.addEventListener("click", (event) => {
+function closeDropdown(event) {
   const clickInsideDropdown = settingsDropdown.contains(event.target);
   const clickOnSettingsButton = settingsButton.contains(event.target);
 
@@ -36,36 +36,30 @@ document.addEventListener("click", (event) => {
   } else if (clickOnSettingsButton) {
     toggleDropdownDisplay();
   }
-});
+}
 
-document
-  .getElementById("user-input")
-  .addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault();
-      document.getElementById("submitBtn").click();
-    }
-  });
+function handleInputKeydown(event) {
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    document.getElementById("submitBtn").click();
+  }
+}
+
+// Event listeners for functions above
+modelToggle.addEventListener("change", handleModelToggle);
+document.addEventListener("click", closeDropdown);
+document.getElementById("user-input").addEventListener("keydown", handleInputKeydown);
 
 window.renderMarkdown = function (content) {
   const md = new markdownit();
   return md.render(content);
 };
 
-function addMessageToDiv(role, content) {
-  let messageDiv = document.createElement("div");
-  messageDiv.className =
-    role === "user" ? "message user-message" : "message assistant-message";
-
-  let renderedContent = window.renderMarkdown(content).trim();
-  messageDiv.innerHTML = renderedContent;
-
-  chatMessagesDiv.appendChild(messageDiv);
-  const codeElements = messageDiv.querySelectorAll("pre code");
+function highlightCode(element) {
+  const codeElements = element.querySelectorAll("pre code");
   codeElements.forEach((codeElement) => {
     hljs.highlightElement(codeElement);
   });
-  autoScroll();
 }
 
 function autoScroll() {
@@ -82,6 +76,54 @@ chatMessagesDiv.addEventListener("scroll", function () {
 
   autoScrollState = isAtBottom;
 });
+
+function addMessageToDiv(role, content = "") {
+  let messageDiv = document.createElement("div");
+  messageDiv.className =
+    role === "user" ? "message user-message" : "message assistant-message";
+
+  let messageText = document.createElement("p");
+  messageDiv.appendChild(messageText);
+
+  if (content) {
+    let renderedContent = window.renderMarkdown(content).trim();
+    messageText.innerHTML = renderedContent;
+    highlightCode(messageDiv);
+  }
+
+  chatMessagesDiv.appendChild(messageDiv);
+  autoScroll();
+
+  return messageText;
+}
+
+function updateSystemMessage(systemMessage) {
+  if (
+    systemMessage &&
+    (!systemMessageRef || systemMessage !== systemMessageRef.content)
+  ) {
+    let systemMessageIndex = messages.findIndex((message) => message.role === "system");
+    // If the system message exists in array, remove it
+    if (systemMessageIndex !== -1) {
+      messages.splice(systemMessageIndex, 1);
+    }
+    systemMessageRef = { role: "system", content: systemMessage };
+    messages.push(systemMessageRef);
+  }
+}
+
+async function postRequest() {
+  return await fetch("/gpt4", {
+    method: "POST",
+    body: JSON.stringify({
+      messages: messages,
+      model_type: modelName,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
 async function handleResponse(response, messageText) {
   const reader = response.body.getReader();
@@ -101,67 +143,28 @@ async function handleResponse(response, messageText) {
     const text = decoder.decode(value);
     assistantMessage += text;
     messageText.innerHTML = window.renderMarkdown(assistantMessage).trim();
-    const codeElements = messageText.querySelectorAll("pre code");
-    codeElements.forEach((codeElement) => {
-      hljs.highlightElement(codeElement);
-    });
+    highlightCode(messageText);
     autoScroll();
   }
 }
 
 window.onload = function () {
-  document
-    .getElementById("chat-form")
-    .addEventListener("submit", async function (event) {
-      event.preventDefault();
+  document.getElementById("chat-form").addEventListener("submit", async function (event) {
+    event.preventDefault();
 
-      let userInput = userInputElem.value.trim();
-      let systemMessage = document.getElementById("system-message").value.trim();
+    let userInput = userInputElem.value.trim();
+    let systemMessage = document.getElementById("system-message").value.trim();
 
-      // Check if the system message has changed
-      if (systemMessage &&
-        (!systemMessageRef || systemMessage !== systemMessageRef.content)
-      ) {
-        // Find the index of the system message in the messages array
-        let systemMessageIndex = messages.findIndex(
-          (message) => message.role === "system"
-        );
+    updateSystemMessage(systemMessage);
 
-        // If the system message exists in the messages array, remove it
-        if (systemMessageIndex !== -1) {
-          messages.splice(systemMessageIndex, 1);
-        }
+    messages.push({ role: "user", content: userInput });
+    addMessageToDiv("user", userInput);
+    userInputElem.value = "";
 
-        // Add new systemMessage to the end of the messages array
-        systemMessageRef = { role: "system", content: systemMessage };
-        messages.push(systemMessageRef);
-      }
+    let messageText = addMessageToDiv("assistant");
 
-      messages.push({ role: "user", content: userInput });
-      addMessageToDiv("user", userInput, "user-input");
+    const response = await postRequest();
 
-      // FIXME: handle this in addMessageToDiv function
-      let messageDiv = document.createElement("div");
-      messageDiv.className = "message assistant-message";
-      let messageText = document.createElement("p");
-      messageDiv.appendChild(messageText);
-      chatMessagesDiv.appendChild(messageDiv);
-      autoScroll();
-
-      const response = await fetch("/gpt4", {
-        method: "POST",
-        body: JSON.stringify({
-          messages: messages,
-          model_type: modelName,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      handleResponse(response, messageText);
-
-      // FIXME: Move this to happen after user input is sent, not after assitant message is received
-      userInputElem.value = "";
-    });
+    handleResponse(response, messageText);
+  });
 };
